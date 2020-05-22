2013924153bec933e88242613f80c140094d9f9c
Luis Hernandez <datboiluiskrrt@gmail.com>
2020-05-22 18:16:37 -0500

    Added styled components

--- portfolioreact/src/components/NavBar.js
+++ portfolioreact/src/components/NavBar.js
@@ -1,26 +1,85 @@
-import React,{useState} from "react";
-import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
+import React from "react";
+import styled from "styled-components";
 
-const NavigationBar = (props) => {
+const Nav = styled.div`
+  background-color: #fff;
+  border-bottom: 1px solid rgba(0,0,0,.0975);
+`
+const NavHeader = styled.div`
+  max-width: 1010px;
+  padding: 26px 20px;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  margin: 0 auto;
+`
+const NavLeft = styled.div`
+  width: 33.333%;
+  text-align: left;
+`
+const NavCenter = styled.div`
+  width: 33.333%;
+  text-align: center;
+`
+const Input = styled.input`
+  font-size: 16px;
+  border: solid 1px #dbdbdb;
+  border-radius: 3px;
+  color: #262626;
+  outline: 0;
+  padding: 7px 33px;
+  border-radius: 3px;
+  color: #999;
+  cursor: text;
+  font-size: 14px;
+  font-weight: 300;
+  text-align: center;
+  background: #fafafa;
+  &:active, &:focus {
+    text-align: left;
+  }
+`
+const NavRight = styled.div`
+  width: 33.333%;
+  text-align: right;
+  svg {
+    margin-right: 20px;
+  }
+`
+const MenuLink = styled.a`
+`
+
+function NavBar () {
+    return (
+      <Nav>
+        <NavHeader>
+          
+          <NavLeft>My Portfolio</NavLeft>
+  
+          <NavCenter>
+            
+          </NavCenter>
+  
+          <NavRight>
+  
+            <MenuLink href="#">
+           
+            </MenuLink>
+  
+            <MenuLink href="#">
+             
+            </MenuLink>
+  
+            <MenuLink href="#">
+             
+            </MenuLink>
+            
+          </NavRight>
+  
+        </NavHeader>
+      </Nav>
+    )
+  }
   
 
-
-  return (
-    <div >
-      <Nav pills>
-        <NavItem>
-          <NavLink href="#" active>My Portfolio</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">Link</NavLink>
-        </NavItem>
-        <NavItem>
-          <NavLink href="#">Another Link</NavLink>
-        </NavItem>
-        
-      </Nav>
-    </div>
-  );
-}
-
-export default NavigationBar;
+export default NavBar;
